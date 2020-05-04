595a1b5c9f9ffcfb61373a2912e2a645b9966133
Danyl Fernandes <androidan1304@gmail.com>
2020-05-04 17:39:23 +0530

    Add updated dotfiles

--- scripts/backup-dotfiles.sh
+++ scripts/backup-dotfiles.sh
@@ -1,1 +1,18 @@
- 
+#!/bin/bash
+
+BASE_CONFIG_PATH=~/.config
+BACKUP_PATH=~/Documents/kde-plasma-setup/dotfiles/
+
+filesToBackup=(
+	"terminator"
+	"gtk-3.0"
+	"gtk-2.0"
+	"Kvantum"
+	"i3"
+	"kdeglobals"
+	"plasma-org.kde.plasma.desktop-appletsrc"
+)
+
+for file in ${filesToBackup[@]}; do
+	cp -r $BASE_CONFIG_PATH/$file $BACKUP_PATH
+done
