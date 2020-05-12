3e8c5826bdb1b283113bbfb5d80da93095cd92d0
Danyl Fernandes <androidan1304@gmail.com>
2020-05-12 22:52:37 +0530

    Add vim config installation

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ then
             |__/      |__/                
     "
 
-    pamac install firefox-developer-edition spotify typora atom visual-studio-code-bin atom android-studio terminator github-cli-git virtualbox virtualbox-host-dkms kazam peek timeshift flameshot balena-etcher kvantum-qt5-git albert vim neofetch unzip
+    pamac install firefox-developer-edition spotify typora atom visual-studio-code-bin atom android-studio terminator github-cli-git virtualbox virtualbox-host-dkms kazam peek timeshift flameshot balena-etcher kvantum-qt5-git albert vim neofetch unzip feh 
 
 else
     echo "Skipping Installation of Apps"
@@ -68,13 +68,13 @@ echo "
 "
 echo -e "\e[1;32mInstalling your fonts...\e[0m"
 
-echo -e "\e[1;34m> Creating .fonts folder in your home dir...\e[0m"
+echo -e "\e[1;34m==> Creating .fonts folder in your home dir...\e[0m"
 mkdir -p ~/.fonts
 
-echo -e "\e[1;34m> Copying your favorite fonts...\e[0m"
+echo -e "\e[1;34m==> Copying your favorite fonts...\e[0m"
 unzip fonts/fonts.zip -d ~/.fonts
 
-echo -e "\e[1;34m> Re-building font cache...\e[0m"
+echo -e "\e[1;34m==> Re-building font cache...\e[0m"
 fc-cache -f -v
 
 # Installation of dotfiles
@@ -90,24 +90,27 @@ echo "
 "
 echo -e "\e[1;32mInstalling your dotfiles...\e[0m"
 
-echo -e "\e[1;34m> Copying GTK-2.0 config...\e[0m"
+echo -e "\e[1;34m==> Copying GTK-2.0 config...\e[0m"
 cp dotfiles/.gtkrc-2.0 ~/
 cp -r dotfiles/gtk-2.0/ ~/.config/
 
-echo -e "\e[1;34m> Copying GTK-3.0 config...\e[0m"
+echo -e "\e[1;34m==> Copying GTK-3.0 config...\e[0m"
 cp -r dotfiles/gtk-3.0/ ~/.config/
 
-echo -e "\e[1;34m> Copying kdeglobals...\e[0m"
+echo -e "\e[1;34m==> Copying kdeglobals...\e[0m"
 cp -r dotfiles/kdeglobals ~/.config/
 
-echo -e "\e[1;34m> Copying KDE Panel config...\e[0m"
+echo -e "\e[1;34m==> Copying KDE Panel config...\e[0m"
 cp -r dotfiles/plasma-org.kde.plasma.desktop-appletsrc ~/.config/
 
-echo -e "\e[1;34m> Copying Kvantum config...\e[0m"
+echo -e "\e[1;34m==> Copying Kvantum config...\e[0m"
 cp -r dotfiles/Kvantum/ ~/.config/
 
-echo -e "\e[1;34m> Copying Terminator config...\e[0m"
+echo -e "\e[1;34m==> Copying Terminator config...\e[0m"
 cp -r dotfiles/terminator/ ~/.config/
+
+echo -e "\e[1;34m==> Copying Vim Config...\e[0m"
+cp dotfiles/.vimrc ~/
 
 # Installation of aurorae themes
 echo "
@@ -125,18 +128,18 @@ echo -e "\e[1;32mInstalling your Aurorae theme...\e[0m"
 mkdir -p ~/.local/share/aurorae/themes/
 cp -r aurorae/Mojave-Dark ~/.local/share/aurorae/themes/
 
-echo -e "\e[1;34m> Copied your Aurorae theme\e[0m"
-echo -e "\e[1;34m> Please set the theme manually by going to...\e[0m"
+echo -e "\e[1;34m==> Copied your Aurorae theme\e[0m"
+echo -e "\e[1;34m==> Please set the theme manually by going to...\e[0m"
 echo -e "\e[1;34m...System Settings > Application Style > Window Decorations > Mojave-dark\e[0m"
 
 # Installation of Global Theme
 echo -e "\e[1;32mInstalling your favorite Global Theme...\e[0m"
 
-echo -e "\e[1;34m> Copying 'Glassy' theme...\e[0m"
+echo -e "\e[1;34m==> Copying 'Glassy' theme...\e[0m"
 mkdir -p ~/.local/share/plasma/desktoptheme/Glassy/
 cp -r themes/Glassy/ ~/.local/share/plasma/desktoptheme/Glassy/
 
-echo -e "\e[1;34m> Copying .kde4 config...\e[0m"
+echo -e "\e[1;34m==> Copying .kde4 config...\e[0m"
 mkdir -p ~/.kde4/share/
 cp -r kde4/share/ ~/.kde4/share
 
@@ -150,10 +153,10 @@ echo "
    | $$   | $$  | $$|  $$$$$$$      | $$$$$$$$| $$  | $$|  $$$$$$$
    |__/   |__/  |__/ \_______/      |________/|__/  |__/ \_______/
 "
-echo -e "\e[1;32m> Installation has finished\e[0m"
-echo -e "\e[1;32m> Reboot now? [y/n]\e[0m"; read reboot 
+echo -e "\e[1;32m==> Installation has finished\e[0m"
+echo -e "\e[1;32m==> Reboot now? [y/n]\e[0m"; read reboot 
 if [$reboot == 'y'] || [$reboot == 'Y']; then
   reboot
 else 
-  echo -e "\e[1;31m> Installation Ended\e[0m"
+  echo -e "\e[1;31m==> Installation Ended\e[0m"
 fi
